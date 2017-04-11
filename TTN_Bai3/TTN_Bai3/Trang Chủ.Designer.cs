56f15aa1806d99f2fe817f5695b449a61618188c
DinhVanDuy <duy.dvd.hs@gmail.com>
2017-04-11 08:08:03 +0700

    cx
    thiết kế form cơ bản

---
+++ TTN_Bai3/TTN_Bai3/Trang Chủ.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TTN_Bai3
+{
+    partial class TrangChủ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbP1 = new System.Windows.Forms.TabPage();
+            this.tbP2 = new System.Windows.Forms.TabPage();
+            this.tbP3 = new System.Windows.Forms.TabPage();
+            this.tbP4 = new System.Windows.Forms.TabPage();
+            this.BT1 = new System.Windows.Forms.Button();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.BT3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tbP1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tbP1);
+            this.tabControl1.Controls.Add(this.tbP2);
+            this.tabControl1.Controls.Add(this.tbP3);
+            this.tabControl1.Controls.Add(this.tbP4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(600, 311);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tbP1
+            // 
+            this.tbP1.Controls.Add(this.pictureBox2);
+            this.tbP1.Controls.Add(this.pictureBox1);
+            this.tbP1.Controls.Add(this.BT3);
+            this.tbP1.Controls.Add(this.BT2);
+            this.tbP1.Controls.Add(this.BT1);
+            this.tbP1.Location = new System.Drawing.Point(4, 22);
+            this.tbP1.Name = "tbP1";
+            this.tbP1.Padding = new System.Windows.Forms.Padding(3);
+            this.tbP1.Size = new System.Drawing.Size(592, 285);
+            this.tbP1.TabIndex = 1;
+            this.tbP1.Text = "Quản Lý Chung";
+            this.tbP1.UseVisualStyleBackColor = true;
+            // 
+            // tbP2
+            // 
+            this.tbP2.Location = new System.Drawing.Point(4, 22);
+            this.tbP2.Name = "tbP2";
+            this.tbP2.Padding = new System.Windows.Forms.Padding(3);
+            this.tbP2.Size = new System.Drawing.Size(592, 285);
+            this.tbP2.TabIndex = 2;
+            this.tbP2.Text = "Quản Lý Mượn Trả";
+            this.tbP2.UseVisualStyleBackColor = true;
+            // 
+            // tbP3
+            // 
+            this.tbP3.Location = new System.Drawing.Point(4, 22);
+            this.tbP3.Name = "tbP3";
+            this.tbP3.Padding = new System.Windows.Forms.Padding(3);
+            this.tbP3.Size = new System.Drawing.Size(592, 285);
+            this.tbP3.TabIndex = 3;
+            this.tbP3.Text = "Tìm Kiếm";
+            this.tbP3.UseVisualStyleBackColor = true;
+            // 
+            // tbP4
+            // 
+            this.tbP4.Location = new System.Drawing.Point(4, 22);
+            this.tbP4.Name = "tbP4";
+            this.tbP4.Padding = new System.Windows.Forms.Padding(3);
+            this.tbP4.Size = new System.Drawing.Size(592, 285);
+            this.tbP4.TabIndex = 4;
+            this.tbP4.Text = "Trợ Giúp";
+            this.tbP4.UseVisualStyleBackColor = true;
+            // 
+            // BT1
+            // 
+            this.BT1.Location = new System.Drawing.Point(39, 162);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(108, 36);
+            this.BT1.TabIndex = 0;
+            this.BT1.Text = "Quản lý sách";
+            this.BT1.UseVisualStyleBackColor = true;
+            // 
+            // BT2
+            // 
+            this.BT2.Location = new System.Drawing.Point(237, 162);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(108, 36);
+            this.BT2.TabIndex = 1;
+            this.BT2.Text = "Quản lý độc giả";
+            this.BT2.UseVisualStyleBackColor = true;
+            // 
+            // BT3
+            // 
+            this.BT3.Location = new System.Drawing.Point(441, 162);
+            this.BT3.Name = "BT3";
+            this.BT3.Size = new System.Drawing.Size(108, 36);
+            this.BT3.TabIndex = 2;
+            this.BT3.Text = "Quản lý danh mục";
+            this.BT3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TTN_Bai3.Properties.Resources.htsoft_131;
+            this.pictureBox2.Location = new System.Drawing.Point(237, 87);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 69);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TTN_Bai3.Properties.Resources.sach1;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 69);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TrangChủ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 311);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "TrangChủ";
+            this.Text = "Trang Chủ";
+            this.tabControl1.ResumeLayout(false);
+            this.tbP1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbP1;
+        private System.Windows.Forms.Button BT3;
+        private System.Windows.Forms.Button BT2;
+        private System.Windows.Forms.Button BT1;
+        private System.Windows.Forms.TabPage tbP2;
+        private System.Windows.Forms.TabPage tbP3;
+        private System.Windows.Forms.TabPage tbP4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+
+    }
+}
